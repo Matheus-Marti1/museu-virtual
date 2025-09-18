816328ea43263437d1de460a7428364d8f29b55c
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2025-09-18 17:01:43 -0300

    Corrigir mais fontes de imagem

--- public/js/timeline-data.js
+++ public/js/timeline-data.js
@@ -19,7 +19,8 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/0/09/Atanasoff-Berry_Computer_%E3%83%BC_Computer_History_Museum_%2830781535612%29.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/0/09/Atanasoff-Berry_Computer_%E3%83%BC_Computer_History_Museum_%2830781535612%29.jpg",
-    source: '<a href="https://commons.wikimedia.org/wiki/File:Atanasoff-Berry_Computer_%E3%83%BC_Computer_History_Museum_(30781535612).jpg">Ik T from Kanagawa, Japan</a>, <a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>, via Wikimedia Commons',
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Atanasoff-Berry_Computer_%E3%83%BC_Computer_History_Museum_(30781535612).jpg">Ik T from Kanagawa, Japan</a>, <a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>, via Wikimedia Commons',
     longDescription: `Em 1937, o professor de física John Atanasoff e o estudante graduado Clifford Berry fizeram a primeira concepção do que seria o então Computador Atanasoff-Berry.
                       O Computador Atanasoff-Berry (ABC, em inglês), foi o primeiro computador eletrônico digital. O dispositivo foi feito para desempenhar uma função específica: para resolver sistemas de equações linear simultâneas.
                       Como ele tinha uma função fixa, sem a possibilidade de mudar o seu programa, o ABC não era um computador de propósito geral. Porém, ele introduziu várias ideias que são fundamentais para computadores modernos, notavelmente usando binário para fazer aritmética, e o uso de eletrônicos ao invés de componentes mecânicos.
@@ -34,7 +35,8 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/e/ee/Zuse-Museum_Huenfeld_Nachbau_Z3_2017.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/e/ee/Zuse-Museum_Huenfeld_Nachbau_Z3_2017.jpg",
-    source: '<a href="https://commons.wikimedia.org/wiki/File:Zuse-Museum_Huenfeld_Nachbau_Z3_2017.jpg">DomenikaBo</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Zuse-Museum_Huenfeld_Nachbau_Z3_2017.jpg">DomenikaBo</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
     longDescription: `Em 12 de maio de 1941, Konrad Zuse conclui a construção do Z3, a primeira máquina de computação digital programável e totalmente automática do mundo.`,
   },
   {
@@ -46,19 +48,20 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/0/07/Harvard_Mark_I_Computer_-_Input-Output_Details.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/1/11/Harvard_Mark_I_Computer_-_Left_Segment.jpg",
-    source: '<a href="https://commons.wikimedia.org/wiki/File:Harvard_Mark_I_Computer_-_Left_Segment.jpg">Daderot at English Wikipedia</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons',
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Harvard_Mark_I_Computer_-_Left_Segment.jpg">Daderot at English Wikipedia</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons',
     longDescription: `Projetado por Howard Aiken e construído pela IBM , o Harvard Mark I foi o primeiro computador digital de larga escala dos EUA capaz de executar cálculos longos automaticamente. Também conhecido como Calculadora Automática de Sequência Controlada (ASCC) da IBM , é considerado por alguns como a primeira calculadora universal.`,
   },
   {
     year: "1946",
     title: "ENIAC",
-    shortDescription:
-      "O primeiro computador eletrônico de grande escala.",
+    shortDescription: "O primeiro computador eletrônico de grande escala.",
     imageCard:
       "https://upload.wikimedia.org/wikipedia/commons/6/6c/ENIAC_Penn1.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/6/6c/ENIAC_Penn1.jpg",
-    source: "Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:ENIAC_Penn1.jpg">The original uploader was TexasDex at English Wikipedia.</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a>, via Wikimedia Commons',
     longDescription: `Em 14 de Fevereiro de 1946, John Mauchly e J. Presper Eckert anunciaram publicamente o ENIAC na Universidade de Pennsylvania.
                       O ENIAC, que significa “Eletronic Numerical Integrator And Computer” (Integrador Numérico Eletrônico e Computador), foi o primeiro computador eletrônico digital de propósito geral.
                       A imprensa dos EUA na época descreveu o ENIAC como:
@@ -68,13 +71,13 @@ export const timelineItems = [
   {
     year: "1946",
     title: "Tubo Williams-Kilburn",
-    shortDescription:
-      "O tubo Williams-Kilburn, o primeiro RAM, é patenteado",
+    shortDescription: "O tubo Williams-Kilburn, o primeiro RAM, é patenteado",
     imageCard:
       "https://upload.wikimedia.org/wikipedia/commons/3/34/Williams-Kliburn_Tube%2C_Manchester_Mark_I.agr.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/3/34/Williams-Kliburn_Tube%2C_Manchester_Mark_I.agr.jpg",
-    source: "ArnoldReinhold, CC BY-SA 4.0, via Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Williams-Kliburn_Tube,_Manchester_Mark_I.agr.jpg">ArnoldReinhold</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
     longDescription: `O tubo Williams-Kilburn, o primeiro RAM, é patenteado`,
   },
   {
@@ -86,7 +89,8 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/3/3c/Manchester_baby_head_on.JPG",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/3/3c/Manchester_baby_head_on.JPG",
-    source: "Geni, CC BY-SA 4.0, via Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Manchester_baby_head_on.JPG">Geni</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
     longDescription: `Em 21 de junho de 1948, na Universidade de Manchester, um computador recém-construído executou com sucesso seu primeiro programa. O computador era a Máquina Experimental de Pequena Escala, também conhecida como "Bebê de Manchester" e "Protótipo Mark 1".`,
   },
   {
@@ -99,7 +103,7 @@ export const timelineItems = [
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/2/22/EDSAC_%2819%29.jpg",
     source:
-      "Copyright Computer Laboratory, University of Cambridge. Reproduced by permission.",
+      'Copyright Computer Laboratory, University of Cambridge. <a href="https://www.cl.cam.ac.uk/relics/archive_photos.html#Copyright_Licencing">Reproduced by permission.</a>',
     longDescription: `A Calculadora Automática de Armazenamento Eletrônico com Atraso (EDSAC) foi o primeiro computador prático de programa armazenado a operar um serviço de computação regular.`,
   },
   {
@@ -148,7 +152,8 @@ export const timelineItems = [
       "https://www.thestoryoftexas.com/upload/images/artifact-spotlight/2014-12-12%20microchip/as_microchip_gallery1.jpg",
     imageModal:
       "https://www.thestoryoftexas.com/upload/images/artifact-spotlight/2014-12-12%20microchip/as_microchip_gallery3.jpg",
-    source: "Texas State History Museum and Texas Instruments",
+    source:
+      '<a href="https://www.thestoryoftexas.com/discover/artifacts/integratedcircuit_spotlight_121214">Texas State History Museum and Texas Instruments</a>',
     longDescription: `Em 1958, Jack Kilby, um engenheiro da Texas Instruments, inventou o primeiro circuito integrado. Essa inovação permitiu que múltiplos componentes eletrônicos fossem miniaturizados e integrados em um único chip de silício, revolucionando a eletrônica e pavimentando o caminho para o desenvolvimento de computadores mais compactos e poderosos.`,
   },
   {
@@ -159,16 +164,20 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/2/23/Spacewar%21-PDP-1-20070512.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/2/23/Spacewar%21-PDP-1-20070512.jpg",
-    source: "Joi Ito from Inbamura, Japan, CC BY 2.0, via Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Spacewar!-PDP-1-20070512.jpg">Joi Ito from Inbamura, Japan</a>, <a href="https://creativecommons.org/licenses/by/2.0">CC BY 2.0</a>, via Wikimedia Commons',
     longDescription: `O DEC PDP-1, lançado em 1960, foi um dos primeiros minicomputadores da história. Ele foi projetado para ser mais acessível e menor do que os computadores mainframe da época, permitindo que mais instituições, como universidades e laboratórios de pesquisa, tivessem acesso à computação. O PDP-1 é frequentemente lembrado por seu papel na popularização da programação interativa e do tempo compartilhado.`,
   },
   {
     year: "1965",
     title: "Lei de Moore",
     shortDescription: "A Lei de Moore é formulada.",
-    imageCard: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Lei_de_moore_2006.png",
-    imageModal: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Lei_de_moore_2006.png",
-    source: "José Maria Silveira Neto, Public domain, via Wikimedia Commons",
+    imageCard:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6c/Lei_de_moore_2006.png",
+    imageModal:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6c/Lei_de_moore_2006.png",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Lei_de_moore_2006.png">José Maria Silveira Neto</a>, Public domain, via Wikimedia Commons',
     longDescription: `A Lei de Moore, formulada por Gordon Moore, cofundador da Intel, é uma observação de que o número de transistores em um chip de computador dobra aproximadamente a cada dois anos, levando a um aumento exponencial na capacidade de processamento e uma redução nos custos. Essa previsão tem se mostrado verdadeira por várias décadas e é um dos principais motores da evolução da tecnologia da informação.`,
   },
   {
@@ -179,7 +188,8 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/0/0e/Intel_logo_%282020%2C_light_blue%29.svg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/0/0e/Intel_logo_%282020%2C_light_blue%29.svg",
-    source: "Intel, Public domain, via Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Intel_logo_(2020,_light_blue).svg">Intel</a>, Public domain, via Wikimedia Commons',
     longDescription: `A Intel Corporation foi fundada em 1968 por Robert Noyce e Gordon Moore, dois pioneiros da indústria de semicondutores. A empresa rapidamente se destacou no desenvolvimento de microprocessadores e memórias, tornando-se uma das principais forças na revolução dos computadores pessoais e na evolução da tecnologia da informação.`,
   },
   {
@@ -187,34 +197,37 @@ export const timelineItems = [
     title: "ARPANET",
     shortDescription: "A ARPANET, precursora da internet, é criada.",
     imageCard:
-      "https://upload.wikimedia.org/wikipedia/commons/6/6f/Arpanet_map_1971.png",
+      "https://upload.wikimedia.org/wikipedia/commons/b/bf/Arpanet_logical_map%2C_march_1977.png",
     imageModal:
-      "https://upload.wikimedia.org/wikipedia/commons/6/6f/Arpanet_map_1971.png",
-    source: "Wikimedia Commons",
+      "https://upload.wikimedia.org/wikipedia/commons/b/bf/Arpanet_logical_map%2C_march_1977.png",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Arpanet_logical_map,_march_1977.png">ARPANET</a>, Public domain, via Wikimedia Commons',
     longDescription: `A ARPANET, ou Advanced Research Projects Agency Network, foi criada em 1969 como um projeto do Departamento de Defesa dos EUA. Ela é considerada a precursora da internet moderna, permitindo a comunicação entre diferentes redes de computadores. A ARPANET introduziu conceitos fundamentais, como o roteamento de pacotes e a arquitetura cliente-servidor, que ainda são utilizados na internet de hoje.`,
   },
   {
     year: "1970",
     title: "Intel 1103",
-    shortDescription: "O primeiro chip de memória DRAM comercialmente disponível do mundo.",
+    shortDescription:
+      "O primeiro chip de memória DRAM comercialmente disponível do mundo.",
     imageCard:
       "https://upload.wikimedia.org/wikipedia/commons/5/59/Intel_C1103.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/5/59/Intel_C1103.jpg",
-    source: '<a href="https://commons.wikimedia.org/wiki/File:Intel_C1103.jpg">Stelo.xyz, Pttn, or Thomas Nguyen</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Intel_C1103.jpg">Stelo.xyz, Pttn, or Thomas Nguyen</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
     longDescription: `O Intel 1103, lançado em outubro de 1970, foi o primeiro chip de memória dinâmica de acesso aleatório (DRAM) disponível comercialmente do mundo. Ele revolucionou a indústria de computadores, permitindo a criação de sistemas mais compactos e eficientes. O 1103 foi amplamente utilizado em computadores pessoais e estações de trabalho durante a década de 1970.
     Com uma memória de 1 kilobit, ele tinha uma capacidade significativamente maior do que as memórias anteriores, que eram baseadas em núcleos magnéticos ou outros tipos de tecnologia.`,
   },
   {
     year: "1971",
     title: "Intel 4004",
-    shortDescription:
-      "O primeiro microprocessador comercialmente disponível.",
+    shortDescription: "O primeiro microprocessador comercialmente disponível.",
     imageCard:
       "https://upload.wikimedia.org/wikipedia/commons/5/52/Intel_4004.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/5/52/Intel_4004.jpg",
-    source: "Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Intel_4004.jpg">LucaDetomi at it.wikipedia</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a>, via Wikimedia Commons',
     longDescription: `Lançado pela Intel, o 4004 foi o primeiro microprocessador comercialmente disponível no mundo. Ele integrou todos os componentes de uma CPU em um único chip de silício, uma inovação que pavimentou o caminho para o desenvolvimento dos computadores pessoais e da eletrônica embarcada.`,
   },
   {
@@ -244,7 +257,8 @@ export const timelineItems = [
       "https://upload.wikimedia.org/wikipedia/commons/b/b9/Apple-lisa-1.jpg",
     imageModal:
       "https://upload.wikimedia.org/wikipedia/commons/b/b9/Apple-lisa-1.jpg",
-    source: "Timothy Colegrove, CC BY-SA 4.0, via Wikimedia Commons",
+    source:
+      '<a href="https://commons.wikimedia.org/wiki/File:Apple-lisa-1.jpg">Timothy Colegrove</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons',
     longDescription: `O Apple Lisa foi uma máquina de 16-bit, baseado no Motorola 68000, que vinha com um HD Winchester de 5Mb e 1Mb de RAM. O computador tinha uma tela monocromática embutida e um drive duplo de disquetes de 5.25 polegadas. Ele foi lançado nos EUA em 19 de janeiro de 1983.
                       Foi o primeiro computador pessoal a utilizar uma Interface Gráfica de Usuário (GUI). Até esse ponto, todos os computadores eram controlados digitando comandos com o teclado. O Lisa vinha com um mouse, permitindo que o usuário apontasse e clicasse em ícones, janelas e menus.`,
   },
